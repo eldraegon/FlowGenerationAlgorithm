0d7bdf210c6a42c8fe8e3a9776dbb7a63b10cf80
eldraegon <50225910+eldraegon@users.noreply.github.com>
2021-02-13 21:53:36 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/FlowGenerationAlgorithm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FlowGenerationAlgorithm.dir/main.cpp.obj"
+  "FlowGenerationAlgorithm.exe"
+  "FlowGenerationAlgorithm.exe.manifest"
+  "FlowGenerationAlgorithm.pdb"
+  "libFlowGenerationAlgorithm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FlowGenerationAlgorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
